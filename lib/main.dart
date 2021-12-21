2efaabe4820460178299f138648cac87ceefde4a
drkspark <madhusudhanrock753@gmail.com>
2021-12-21 19:54:00 +0530

    Basic Working with Column and Rows

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,44 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  // const App({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blueAccent[400],
-        body: Column(
+      body: Container(
+        //! Devices Height and Width
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+
+        child: Column(
+          //! This aligns the children to the center of the row
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          //! This aligns the children to the center of the column
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset("images/logo.png"),
-            Text("First App",
-                style: TextStyle(
-                    fontSize: 32,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.indigo[800]))
+            Text("Madhu Sudhan"),
+            SizedBox(
+              width: 30,
+              height: 50,
+            ),
+            Row(
+              
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+                Text("First"),
+                SizedBox(width:15),
+                Text("Second")
+              ],
+            ),
+            Text("Soumya"),
+            Text("Janani"),
+            Text("Vitesh"),
+            Text("Abhinay")
+
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
