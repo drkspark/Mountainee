74777be9f02adf27c4615c9e4370e6222bf68a36
drkspark <madhusudhanrock753@gmail.com>
2021-12-21 20:01:12 +0530

    How the  maxAxisSize works in row

--- lib/main.dart
+++ lib/main.dart
@@ -22,21 +22,32 @@ class App extends StatelessWidget {
 
           //! This aligns the children to the center of the column
           mainAxisAlignment: MainAxisAlignment.center,
+          
+          
           children: [
             Text("Madhu Sudhan"),
             SizedBox(
               width: 30,
               height: 50,
             ),
-            Row(
-              
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                
-                Text("First"),
-                SizedBox(width:15),
-                Text("Second")
-              ],
+            Container(
+                color: Colors.red,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [Text("Row1"),Text("Row2")],
+                  ),
+                  Row(
+                    children: [Text("Row2"),Text("Row2.2")],
+                  ),
+                  Text("This is the 3rd Widget")
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 30,
+              height: 50,
             ),
             Text("Soumya"),
             Text("Janani"),
